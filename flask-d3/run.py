5ba693b879c881bf6fa0f1c52f4ad2d8fbf43186
Sean Watland <51850964+swatland610@users.noreply.github.com>
2021-03-11 20:32:45 -0600

    merge add_json_file branch to main

--- flask-d3/run.py
+++ flask-d3/run.py
@@ -1,15 +1,18 @@
-from flask import Flask, jsonify, render_template
+import os
 import pandas as pd
 import json
-# import my extract 10 year budget script
-from data.extract_budget_data import Extract
+from flask import Flask, jsonify, render_template
+
 
 app = Flask(__name__)
 # this allows me to see changes I make in real time
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
 # pull in json data
-ten_year_budgets = Extract().extract_budgets
+JSON_PATH = os.path.join(app.static_folder, 'data', 'ten_year_budgets.json')
+
+with open(JSON_PATH) as data:
+    ten_year_budgets = json.load(data)
 
 @app.route('/')
 def index():
