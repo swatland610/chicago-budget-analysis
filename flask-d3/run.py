a378c1fbe778e1dbe71a1993af4721c1a7656072
Sean Watland <51850964+swatland610@users.noreply.github.com>
2021-03-11 21:04:26 -0600

    removed elements that loaded data at run

--- flask-d3/run.py
+++ flask-d3/run.py
@@ -8,15 +8,9 @@ app = Flask(__name__)
 # this allows me to see changes I make in real time
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
-# pull in json data
-JSON_PATH = os.path.join(app.static_folder, 'data', 'ten_year_budgets.json')
-
-with open(JSON_PATH) as data:
-    ten_year_budgets = json.load(data)
-
 @app.route('/')
 def index():
-    return render_template('analysis.html', data=ten_year_budgets)
+    return render_template('analysis.html')
 
 
 if __name__=='__main__':
