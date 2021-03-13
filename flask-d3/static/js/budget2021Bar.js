e919a2e59f55227a9a586c866207246792515869
Sean Watland <51850964+swatland610@users.noreply.github.com>
2021-03-13 13:23:19 -0600

    add total for 2021 budget

--- flask-d3/static/js/budget2021Bar.js
+++ flask-d3/static/js/budget2021Bar.js
@@ -1,6 +1,6 @@
 // pulling full json file
 // initiate empty array to create new object
-const budget2021 = [];
+var budget2021 = [];
 
 // extract data if it's from 2021
 d3.json("/static/data/ten_year_budgets.json", function(data) {
@@ -8,11 +8,17 @@ d3.json("/static/data/ten_year_budgets.json", function(data) {
         if(data[i].budget_year === 2021) {
             var lineItem2021 = {
                 department:data[i].department_description,
-                amount:data[i].department_description
+                amount:data[i].amount
             };
             // going to push created line item to budget2021
             budget2021.push(lineItem2021);
         }
     }
 });
+
+// now that we have just the line items from 2021, let's group totals by department & calculate percentage
+// calculate total budget to calculate % of totals by department
+const totalBudget2021 = budget2021.reduce(function(previous, current) { 
+    return previous + current.amount
+}, 0);
 
