5edcddfcb301d679bbfdb686c9c1f663bccf7617
Sean Watland <51850964+swatland610@users.noreply.github.com>
2021-03-26 00:46:40 -0500

    add annual budget totals wrangler

---
+++ dash/data/wranglers/viz_data.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import json 
+
+class Data_Viz_Data:
+    def __init__(self):
+        # just pull all ten years as a Dataframe
+        self.ten_year_budgets = pd.read_csv('../ten_year_budgets.csv', index_col="Unnamed: 0")
+
+    # total budget for each year
+    def get_annual_budget_totals(self):
+        data = Data_Viz_Data().ten_year_budgets 
+        annual_budgets_totals = data.groupby('budget_year').agg({'amount':'sum'})
+        return annual_budgets_totals
