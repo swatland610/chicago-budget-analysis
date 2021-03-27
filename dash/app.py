97b298bbf4aa60881fe3603f5759f24d6586635c
Sean Watland <51850964+swatland610@users.noreply.github.com>
2021-03-27 11:42:13 -0500

    add annual totals chart

---
+++ dash/app.py
@@ -0,0 +1,35 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import plotly.express as px
+import pandas as pd
+
+# import data wranglers
+from data.wranglers.viz_data import Data_Viz_Data
+
+# initiate dash app
+app = dash.Dash(__name__)
+
+# set data objects
+annual_budgets_totals = Data_Viz_Data().get_annual_budget_totals()
+
+# pull in budget data
+annual_totals_fig = px.area(annual_budgets_totals, x=annual_budgets_totals.index, y='amount')
+
+app.layout = html.Div(children = [
+    html.H1(children='''
+    Chicago Budgets: 10 Year Look
+'''), 
+    
+    html.H2(children='''
+    Annual Budget Totals
+    '''
+    ), 
+    
+    dcc.Graph(
+        figure = annual_totals_fig
+    )])
+
+if __name__ == '__main__':    
+    app.run_server(debug=True)
+
--- dash/data/wranglers/viz_data.py
+++ dash/data/wranglers/viz_data.py
@@ -4,10 +4,11 @@ import json
 class Data_Viz_Data:
     def __init__(self):
         # just pull all ten years as a Dataframe
-        self.ten_year_budgets = pd.read_csv('../ten_year_budgets.csv', index_col="Unnamed: 0")
+        self.ten_year_budgets = pd.read_csv('data/ten_year_budgets.csv', index_col="Unnamed: 0")
 
     # total budget for each year
     def get_annual_budget_totals(self):
         data = Data_Viz_Data().ten_year_budgets 
         annual_budgets_totals = data.groupby('budget_year').agg({'amount':'sum'})
         return annual_budgets_totals
+    
